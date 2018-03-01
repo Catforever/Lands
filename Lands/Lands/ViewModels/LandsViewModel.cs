9c7e8cbb754834ce436b2df8212c230366c60eb4
Catforever <isaias_samuel.1987@hotmail.com>
2018-03-01 14:53:33 -0600

    List view lands personalation

--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -7,14 +7,21 @@ namespace Lands.ViewModels
     using Models;
     using Xamarin.Forms;
     using System.Collections.Generic;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using System.Linq;
 
-    public class LandsViewModel :BaseViewModel
+    public class LandsViewModel : BaseViewModel
     {
         #region Services
         private ApiService apiService;
         #endregion
+
         #region Attributes
         private ObservableCollection<Land> lands;
+        private bool isRefreshing;
+        private string filter;
+        private List<Land> landsList;
         #endregion
 
         #region Properties
@@ -23,6 +30,23 @@ namespace Lands.ViewModels
             get { return this.lands; }
             set { SetValue(ref this.lands, value); }
         }
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+        public string Filter
+        {
+            get { return this.filter; }
+            set
+            {
+                SetValue(ref this.filter, value);
+                this.Search();
+            }
+        }
+
+
+
         #endregion
 
         #region Constructors
@@ -36,10 +60,12 @@ namespace Lands.ViewModels
         #region Methods
         private async void LoadLands()
         {
+            this.IsRefreshing = true;
             var connection = await this.apiService.CheckConnection();
 
             if (!connection.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
                     connection.Message,
@@ -54,6 +80,7 @@ namespace Lands.ViewModels
                 "/v2/all");
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
                     response.Message,
@@ -62,9 +89,47 @@ namespace Lands.ViewModels
                 return;
             }
 
-            var List = (List<Land>)response.Result;
-            this.Lands = new ObservableCollection<Land>(List);
+            this.landsList = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(this.landsList);
+            this.IsRefreshing = false;
         }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+
+        }
+
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+        }
+
+        private void Search()
+        {
+            if (string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landsList);
+            }
+            else
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landsList.Where(
+                        l =>l.Name.ToLower().Contains(this.Filter.ToLower())  ||
+                            l.Capital.ToLower().Contains(this.Filter.ToLower())
+                        ));
+            }
+        }
+
         #endregion
     }
 }
